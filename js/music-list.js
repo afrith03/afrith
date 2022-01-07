10703ee445b103ccb8684c1ec43eec5c4fa1d1ec
afrith03 <97300002+afrith03@users.noreply.github.com>
2022-01-07 21:44:10 +0500

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "vaaji vaaji",
+    artist: "shreya saran,rajni",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "sahaana",
+    artist: "shreya saran",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Excuse me ",
+    artist: "Shreya saran, Vikram",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "kannukul yeadho",
+    artist: "Dhanush, shreya saran",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "yethi yethi",
+    artist: "Surya",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Rakita Rakita",
+    artist: "Dhanush",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "urvasi urvasi",
+    artist: "A.R rahman",
+    img: "music-7",
+    src: "music-7"
+  }
+
+];
